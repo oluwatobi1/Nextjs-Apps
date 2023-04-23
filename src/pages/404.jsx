8433ea0f91d69f0a540be3c9dde75187192528bc
oluwatobi1 <akintunlesetobi@gmail.com>
2023-04-23 15:43:53 +0100

    feat: add 404 page

---
+++ src/pages/404.jsx
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <div className="not-found">
+        <h1>Oooops....</h1>
+        <h2>That page cannot be found.</h2>
+        <p>Go back to the <Link href="/">Homepage</Link></p>
+    </div>
+  )
+}
+
+export default NotFound
